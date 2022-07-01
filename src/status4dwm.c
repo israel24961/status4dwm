5972ea1f073d0eff3bd1920a0e9dab62ca593a89
I24961 <israel2461@hotmail.com>
2022-07-01 14:32:27 +0200

    status4dwm 2 stats

--- src/status4dwm.c
+++ src/status4dwm.c
@@ -7,8 +7,6 @@
 int main()
 {
     stat_stuff* st=stat_init();
-    stat_add(st,2.0f,task_2secs);
-    stat_add(st,5.0f,task_5secs);
     stat_add(st,3.0f,get_cpu_usage);
     stat_add(st,5.0f,get_xmr);
     while(1) {
